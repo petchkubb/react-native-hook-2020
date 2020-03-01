37b3d001d834dc33e78bbc1c9d2b058d6f3aa600
Petch <petchkubb@live.com>
2020-03-01 08:09:27 +0700

    learn textinput

--- src/pages/TextScreen/index.js
+++ src/pages/TextScreen/index.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
+	const [password, setPaswword] = useState('')
 	return (
 		<View>
+			<Text>Enter Password</Text>
 			<TextInput
 				style={styles.input}
 				autoCapitalize="none"
 				autoCorrect={false}
+				value={password}
+				onChangeText={text => setPaswword(text)}
 			/>
+			{password.length < 5 && (
+				<Text>Password must be longer than 5 characters</Text>
+			)}
 		</View>
 	)
 }
